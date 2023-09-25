58d6efb26ab4d04771e3b3da680fc4c1eb17f8f5
JoshuaSiyame <josh2023decoded@gmail.com>
2023-09-25 21:12:00 +0300

    Shop commits

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -5,6 +5,18 @@ const Shop = () => {
   return (
     <div id="shop">
       <div id='shop-container'>
+        <ol id='shop-info-list'>
+          <li className='shop-info-list-item' id='left-shop-item'>Showing: 15/20 Items</li>
+          <li className='shop-info-list-item' id='right-shop-item'>
+            <select name='item-sorting'>
+              <option value="">Sort By:</option>
+              <option value="latest">Latest</option>
+              <option value="old">Old</option>
+              <option value="priceLowHigh"><small>Low-High Price</small></option>
+              <option value="priceHighLow"><small>High-Low Price</small></option>
+            </select>
+          </li>
+        </ol>
       </div>
     </div>
   );
